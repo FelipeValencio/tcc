0536241008032f0d59f31d2e8a85dc44566826e1
Felipe Valencio <fgv.valencio@gmail.com>
2024-05-08 21:08:10 -0300

    algoritmo pronto

--- estado.py
+++ estado.py
@@ -8,31 +8,31 @@ def ler_tabela(nome_arquivo):
     return tabela
 
 
-def maquina_de_estado(tabelaEstado):
+def maquina_de_estado(tabela_estado):
+    controles = []
     estado_atual = 1
-    while estado_atual is None or (isinstance(estado_atual, int) and estado_atual < len(tabelaEstado)):
-        pergunta = tabelaEstado[estado_atual][1]
-        controles_sim = tabelaEstado[estado_atual][2]
-        controles_nao = tabelaEstado[estado_atual][3]
+    while (estado_atual is not None or isinstance(estado_atual, int)) and estado_atual < len(tabela_estado):
+        print("Estado atual: " + str(estado_atual))
+        pergunta = tabela_estado[estado_atual][1]
+        controles_sim = tabela_estado[estado_atual][2]
+        controles_nao = tabela_estado[estado_atual][3]
         print("Pergunta:", pergunta)
         resposta = input("Responda com 'Sim' ou 'Não': ").lower()
 
         if resposta == 'sim':
-            controles = controles_sim
-            print(tabelaEstado[estado_atual][4])
-            estado_atual = encontrar_indice_por_id(tabela, tabelaEstado[estado_atual][4])
-        elif resposta == 'não':
-            controles = controles_nao
-            estado_atual = encontrar_indice_por_id(tabela, tabelaEstado[estado_atual][5])
+            [controles.append(c.strip()) for c in controles_sim.split("\n") if c.strip() not in controles]
+            print(tabela_estado[estado_atual][4])
+            estado_atual = encontrar_indice_por_id(tabela_estado, tabela_estado[estado_atual][4])
+        elif resposta == 'não' or resposta == 'nao':
+            [controles.append(c.strip()) for c in controles_nao.split("\n") if c.strip() not in controles]
+            estado_atual = encontrar_indice_por_id(tabela_estado, tabela_estado[estado_atual][5])
         else:
             print("Resposta inválida. Por favor, responda com 'Sim' ou 'Não'.")
             continue
 
-        print("Controles aplicáveis:", controles)
+        print("Controles aplicáveis:\n", controles)
         print("\n")
 
-
-tabela = ler_tabela('estados_controles.csv')
 
 def encontrar_indice_por_id(tabela, id_desejado):
     for indice, linha in enumerate(tabela):
@@ -40,4 +40,5 @@ def encontrar_indice_por_id(tabela, id_desejado):
             return indice
     return None
 
-maquina_de_estado(tabela)
+
+maquina_de_estado(ler_tabela('tabela_estado.csv'))
